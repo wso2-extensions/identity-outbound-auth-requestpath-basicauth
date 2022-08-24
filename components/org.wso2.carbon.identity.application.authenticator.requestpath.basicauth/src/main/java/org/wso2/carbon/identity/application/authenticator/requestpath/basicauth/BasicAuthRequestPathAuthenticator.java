b1808529fd677a4a85f745e124457dfb0ecd6183
Thamindu Dilshan Jayawickrama <35653110+ThaminduDilshan@users.noreply.github.com>
2022-08-24 11:51:44 +0530

    Revert "fix: rename basic authn displayName"

--- components/org.wso2.carbon.identity.application.authenticator.requestpath.basicauth/src/main/java/org/wso2/carbon/identity/application/authenticator/requestpath/basicauth/BasicAuthRequestPathAuthenticator.java
+++ components/org.wso2.carbon.identity.application.authenticator.requestpath.basicauth/src/main/java/org/wso2/carbon/identity/application/authenticator/requestpath/basicauth/BasicAuthRequestPathAuthenticator.java
@@ -157,7 +157,7 @@ public class BasicAuthRequestPathAuthenticator extends AbstractApplicationAuthen
 
     @Override
     public String getFriendlyName() {
-        return "Basic Auth";
+        return "basic-auth";
     }
 
     @Override
